de58d7e3ad03d1c3f9a93cbb386fff2ba1559f9d
jbarrette9214 <jpb0800@yahoo.com>
2019-08-21 22:19:51 -0400

    Initial commit

---
+++ DesktopApp/src/main/java/com/barrette/tireinventory/DesktopApp/App.java
@@ -0,0 +1,13 @@
+package com.barrette.tireinventory.DesktopApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
